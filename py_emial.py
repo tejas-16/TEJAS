1274932bab2a2b00bedc88b84c2b2db6d6a2f110
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2024-04-21 17:08:57 +0530

    Add files via upload

---
+++ py_emial.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(subject, message, from_email, to_email, smtp_server, smtp_port, smtp_username, smtp_password):
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    server.quit()
+
+# Example usage:
+send_email("Subject: Unix Fastack Sanity Checkbject", "Hello Team,/Please help me with the sanity check.", "tejasvinchurkar16@example.com", "tejasvinchurkar.scoe.comp@example.com", "smtp.gmail.com", 587, "smtp_username", "smtp_password")
+
+
+
+    
+    """
+    msg = MIMEMultipart()
+    msg['tejasvinchurkar16@gmail.com'] = from_email
+    msg['tejasvinchurkar.scoe.comp@gmail.com'] = to_email
+    msg['Subject: Unix Fastack Sanity Checkbject'] = subject
+    msg.attach(MIMEText(message, "Hello Team, /Please help me with the sanity check."))
+    """
