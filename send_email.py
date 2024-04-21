25e28d7d1f72a130c7d71dca562cbe70b510dcaa
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2024-04-21 16:47:22 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,22 +1,23 @@
-import smtplib, ssl
+import smtplib
+import ssl
 import os
 
 port = 465
 smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
+recipient_email = "tejasvinchurkar16@gmail.com"  # Replace with the recipient's email address
 
-message = """ 
+message = """
 Subject: Unix Fastack Sanity Check
-
 
 Hello Team, 
 
 Please help me with the sanity check.
 """
+
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(smtp_server, port, context =context) as server:
-    server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,PASSWORD,message)   
-    
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(USERNAME, PASSWORD)
+    server.sendmail(USERNAME, recipient_email, message)
