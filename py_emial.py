54db398ff7405e60ece719e05ece6b52600ea395
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2024-04-21 17:16:19 +0530

    Update py_emial.py

--- py_emial.py
+++ py_emial.py
@@ -3,6 +3,12 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def send_email(subject, message, from_email, to_email, smtp_server, smtp_port, smtp_username, smtp_password):
+    msg = MIMEMultipart()
+    msg['tejasvinchurkar16@gmail.com'] = from_email
+    msg['tejasvinchurkar.scoe.comp@gmail.com'] = to_email
+    msg['Subject: Unix Fastack Sanity Checkbject'] = subject
+    msg.attach(MIMEText(message, "Hello Team, /Please help me with the sanity check."))
+
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()
     server.login(smtp_username, smtp_password)
@@ -15,10 +21,5 @@ send_email("Subject: Unix Fastack Sanity Checkbject", "Hello Team,/Please help m
 
 
     
-    """
-    msg = MIMEMultipart()
-    msg['tejasvinchurkar16@gmail.com'] = from_email
-    msg['tejasvinchurkar.scoe.comp@gmail.com'] = to_email
-    msg['Subject: Unix Fastack Sanity Checkbject'] = subject
-    msg.attach(MIMEText(message, "Hello Team, /Please help me with the sanity check."))
-    """
+    
+   
