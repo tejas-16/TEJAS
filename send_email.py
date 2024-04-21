a54dbd5b96abf3fb6412ee1ded5dfe223ac8ecf7
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2024-04-21 16:23:39 +0530

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+import os
+
+port = 465
+smtp_server = "smtp.gmail.com"
+USERNAME = os.environ.get('USER_EMAIL')
+PASSWORD = os.environ.get('USER_PASSWORD')
+
+message = """\ 
+Subject: Unix Fastack Sanity Check
+
+
+Hello Team, 
+
+Please help me with the sanity check.
+"""
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context =context) as server:
+    server.loginin(USERNAME,PASSWORD)
+    server.sendmail(USERNAME,PASSWORD,message)   
+    
